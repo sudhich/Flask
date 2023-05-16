61fe553580d6d2570cb26e0c2bb93e03a51757fd
SUDHIRAM CHAUHAN <70084967+sudhich@users.noreply.github.com>
2023-05-16 11:30:37 +0530

    Add files via upload

---
+++ flask_demo/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__) #object app and class Flask
+
+#object.method[function()] app.route()
+@app.route('/', methods=['GET', 'POST']) # To render Homepage
+def home_page():#1st reach out url then excute function.If not reach out url then not excute function.
+    return render_template('index.html')
+#object.method[function()] app.route()
+@app.route('/math', methods=['POST'])  # This will be called from UI
+def math_operation():
+    if (request.method=='POST'):
+        operation=request.form['operation']
+        num1=int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if(operation=='add'):
+            r=num1+num2
+            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+        if (operation == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'multiply'):
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'divide'):
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        return render_template('results.html',result=result)
+#object.method[function()] app.route()
+@app.route('/via_postman', methods=['POST']) # for calling the API from Postman/SOAPUI
+def math_operation_via_postman():
+    if (request.method=='POST'):
+        operation=request.json['operation']
+        num1=int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if(operation=='add'):
+            r=num1+num2
+            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+        if (operation == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'multiply'):
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'divide'):
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        return jsonify(result)
+@app.route('/sudh_function')
+def url_test1():
+    test1=request.args.get('val1')
+    test2=request.args.get('val2')
+    result=int(test1)+int(test2)
+    return '''<h1> My result is: {} </h1>'''.format(result)
+#Build the flask application using the following command.
+if __name__ == '__main__':
+    app.run()
